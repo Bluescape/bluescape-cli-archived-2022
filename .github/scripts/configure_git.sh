b77077fed8cc439052dffe6a1b6dcc2b9d6c16df
safi <sathishkumar.sethu@bluescape.com>
2022-06-27 15:25:25 +0530

    updated

--- .github/scripts/configure_git.sh
+++ .github/scripts/configure_git.sh
@@ -1,4 +1,4 @@
 # Configure git for the github action bot to allow pushing to the repo
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote set-url origin https://x-access-token:${GITHUBPAT}@github.com/bluescape/infrastructure
+git remote set-url origin https://x-access-token:${GITHUBPAT}@github.com/bluescape/bluescape-cli

f60539db34f134fe870e3bfebfa60be195761e68
Rakshitha-Sheshagiri <rakshitha.sheshagiri@bluescape.com>
2022-11-18 00:30:04 +0530

    cli for isam legacy subscription

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,10 @@
-import { ID_REGEX } from '../constants';
+import { EXTERNAL_SUBSCRIPTION_ID_REGEX, ID_REGEX } from '../constants';
 
 export const isId = (id: unknown): boolean =>
   typeof id === 'string' && ID_REGEX.test(id);
 
-  export const valueExists = (value: unknown): boolean =>
+export const valueExists = (value: unknown): boolean =>
   value !== null && value !== undefined;
+
+export const isExternalSubscriptionId = (id: unknown): boolean =>
+  typeof id === 'string' && EXTERNAL_SUBSCRIPTION_ID_REGEX.test(id);
